84ca14ff73571e63ee3771c11a1ad09742301c36
sammed.sankonatti <sammed.sankonatti18@gmail.com>
2025-03-28 23:36:31 +0530

    removed .next

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"arKSvkPu4uw04iF108GfHp1mpo9OTrHboJufqtoEPVY=\"\n}"
---
+++ .next/static/chunks/src_app_layout_f0e4c1a2.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/src_app_layout_f0e4c1a2.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/src_app_layout_f0e4c1a2.js",
+  "chunks": [
+    "static/chunks/[root of the server]__860c0698._.css"
+  ],
+  "source": "dynamic"
+});
